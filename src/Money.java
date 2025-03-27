240a169039363c06381020ea2971bb6bd5e3664f
EriveltonJr <franciscoerivelton12@gmail.com>
2025-03-27 04:24:42 -0300

    Etapa 4 - Dollar e Franc unificados na classe Money

--- src/Money.java
+++ src/Money.java
@@ -1,19 +1,31 @@
-abstract class Money {
+public class Money {
     protected int amount;
+    protected String currency;
 
-    static Money dollar(int amount) {
-        return new Dollar(amount);
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
     }
 
-    static Money franc(int amount) {
-        return new Franc(amount);
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
     }
 
-    abstract Money times(int multiplier);
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
+    public String currency() {
+        return currency;
+    }
 
     @Override
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount;
+        return amount == money.amount && currency.equals(money.currency());
     }
 }
